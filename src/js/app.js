2499c082c148edfbff10ef9f2aab3bc28f1d3e26
Violen Amanda <violenamanda21@gmail.com>
2020-11-16 00:52:40 +0700

    rebuild

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
-require('animate.css')
+try {
+    window.AOS = require('aos');
+
+    // Mengambil tinggi body pada browser
+    const tinggi = $(window).height() / 2;
+
+    // Membuat fungsi scroll ditampilkan Tombol Back-top
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > tinggi) {
+            $('#back-top').addClass('active');
+        } else {
+            $('#back-top').removeClass('active');
+        }
+    });
+} catch (e) {}
